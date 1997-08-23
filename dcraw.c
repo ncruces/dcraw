bf8271cc4c9a83b1f6919095180b5366a5013b41
dcoffin <dcoffin>
1997-08-23 19:36:52 +0000

    Version 0.4, just for testing A breakthrough!  I finally figured out what the "mystery pixels" are.

--- dcraw.c
+++ dcraw.c
@@ -1,7 +1,7 @@
 /*
-   CRW to PPM converter Version 0.3
+   CRW to PPM converter Version 0.4
 
-   by Dave Coffin (dcoffin at shore dot net)  3/24/97
+   by Dave Coffin (dcoffin at shore dot net)  8/24/97
 
    No rights reserved.  Do what you want with this code,
    but I accept no responsibility for any consequences
@@ -14,95 +14,172 @@
 #include <stdio.h>
 #include <string.h>
 
-typedef unsigned char uchar;
+/*  Create extra files with debugging info */
 
-/* This array holds the raw pixel values */
-uchar pgm[613][854];
+#define DEBUG
 
-/*
-   Write the raw pixel values to a PGM file before
-   RGB conversion.  Useful for debugging.
-*/
-#define WANT_PGM_FILE
+/* Use these to adjust the final color balance */
+
+#define RED_MUL 1.0
+#define GRN_MUL 0.5
+#define BLU_MUL 1.0
 
 /* DOS likes to trash binary files!! */
+
 #ifndef O_BINARY
 #define O_BINARY 0
 #endif
 
 #define WFLAGS O_WRONLY | O_CREAT | O_TRUNC | O_BINARY
 
-/* Read a CRW file into the pgm[] array */
-read_crw(int in)
-{
-  uchar data[1120];
-  int bin, irow, icol, orow, ocol;
+typedef unsigned char  uchar;
 
-  read (in, data, 24);				/* Chop the header */
-  if (memcmp(data+4,"HEAPCCDR",8))		/* Make sure it's CRW */
-    return 1;
+/* This 4MB array holds the GMCY values for each pixel */
+
+ushort gmcy[613][854][4];
+
+/* Creates a new filename with a different extension */
+exten(char *new, char *old, char *ext)
+{
+  char *cp;
+
+  strcpy(new,old);
+  cp=strrchr(new,'.');
+  if (!cp) cp=new+strlen(new);
+  strcpy(cp,ext);
+}
+
+#ifdef DEBUG
+debug (char *arg, char *ext)
+{
+  char fname[64];
+  int fd;
+
+  exten(fname,arg,ext);
+  fd = open(fname,WFLAGS,0644);
+  if (fd < 0)
+  { perror(fname);
+    fd = open("/dev/null",O_WRONLY);
+  }
+  return fd;
+}
+#endif
+
+/*
+   Returns the color of a given pixel.  The pattern goes:
+
+(0,0)->	G M G M G M	row 0
+	C Y C Y C Y	row 1
+	M G M G M G	row 2
+	C Y C Y C Y	row 3
+*/
+color (unsigned x, unsigned y)
+{
+  int color;
+
+  color = ((y & 1) << 1) + (x & 1);
+  if ((y & 3) == 2)		/* 0=green, 1=magenta, 2=cyan, 3=yellow */
+    color ^= 1;			/* Swap green and magenta on row 2 */
+  return color;
+}
+
+/* Read a CRW file into the pgm[] array */
+read_crw(char *arg)
+{
+  uchar data[1120], *dp, *gp;
+  int fd, irow, icol, orow, ocol;
+
+#ifdef DEBUG
+  int gfd, gcol, mfd, mcol;
+  uchar grid[896], myst[224];
+
+  gfd = debug(arg,".grid");
+  mfd = debug(arg,".myst");
+
+  write(gfd,"P5\n896 613\n255\n",15);
+  write(mfd,"P4\n1792 613\n",12);
+#endif
+
+  fd = open(arg,O_RDONLY | O_BINARY);
+  if (fd < 0)
+  { perror(arg);
+    return 0; }
+
+  read (fd, data, 26);				/* Check the header */
+  if (memcmp(data,"MM",2) || memcmp(data+6,"HEAPCCDR",8))
+  {
+    fprintf(stderr,"%s is not a CRW file.\n",arg);
+    return 0;
+  }
 
   for (irow=orow=0; irow < 613; irow++)
   {
-    read (in, data, 1120);                      /* Read one row */
+    read (fd, data, 1120);                      /* Read one row */
+
+#ifdef DEBUG
+    for (dp=data, gp=grid; dp < data+1120; dp+=10, gp+=8)
+    {
+/* wrong */
+      gp[0]=(dp[0] << 2) + (dp[1] >> 4 & 3);
+      gp[1]=(dp[2] << 2) + (dp[1] >> 2 & 3);
+      gp[2]=(dp[3] << 2) + (dp[1] >> 0 & 3);
+      gp[3]=(dp[4] << 2) + (dp[1] >> 6 & 3);
+      gp[4]=(dp[5] << 2) + (dp[9] >> 0 & 3);
+      gp[5]=(dp[6] << 2) + (dp[9] >> 2 & 3);
+      gp[6]=(dp[7] << 2) + (dp[9] >> 4 & 3);
+      gp[7]=(dp[8] << 2) + (dp[9] >> 6 & 3);
+
+#if 0
+/* right */
+      gp[0]=(dp[0] << 2) + (dp[1] >> 6 & 3);
+      gp[1]=(dp[2] << 2) + (dp[1] >> 4 & 3);
+      gp[2]=(dp[3] << 2) + (dp[1] >> 2 & 3);
+      gp[3]=(dp[4] << 2) + (dp[1] >> 0 & 3);
+      gp[4]=(dp[5] << 2) + (dp[9] >> 0 & 3);
+      gp[5]=(dp[6] << 2) + (dp[9] >> 2 & 3);
+      gp[6]=(dp[7] << 2) + (dp[9] >> 4 & 3);
+      gp[7]=(dp[8] << 2) + (dp[9] >> 6 & 3);
+#endif
+    }
+    lseek (gfd, orow*896+15, 0);
+    write (gfd, grid, 896);
+    lseek (mfd, orow*224+12, 0);
+    write (mfd, myst, 224);
+#endif
+
+#if 0
+    for (icol=gcol=mcol=0; icol < 1120; icol++)
+    {
+      if ( icol % 10 != 1 && icol % 10 != 9 )
+	grid[gcol++] = data[icol];		/* Data pixels go here */
+      else
+	myst[mcol++] = data[icol];		/* Mystery pixels go here */
+    }
+    lseek (gfd, orow*896+15, 0);
+    write (gfd, grid, 896);
+    lseek (mfd, orow*224+12, 0);
+    write (mfd, myst, 224);
+#endif
+
     for (icol=ocol=0; icol < 1067; icol++)
     {
-      if ( icol % 10 != 1 && icol % 10 != 9 ) /* Delete the "mystery pixels" */
-	pgm[orow][ocol++] = data[icol];
+      if ( icol % 10 != 1 && icol % 10 != 9 )
+      {
+	gmcy[orow][ocol][color(orow,ocol)] = (ushort) data[icol] << 6;
+	ocol++;
+      }
     }
     if ((orow+=2) > 612)        /* Once we've read all the even rows, */
       orow = 1;                 /* read the odd rows. */
   }
-  close(in);
-  return 0;                     /* Success */
-}
+  close(fd);
 
-/* Used for parsing PGM headers */
-int get_num(FILE *fp)
-{
-  int num=0;
-  char c;
-
-  for (;;)
-  {
-    if (isspace(c=getc(fp)))		/* Ignore whitespace */
-      continue;
-    if ( c=='#' )			/* Ignore comments */
-      while (getc(fp) != '\n');
-    else if (isdigit(c))		/* Grab digits */
-    { do {
-	num = num*10 + (c - '0');	/* Make a number */
-	c=getc(fp);
-      } while (isdigit(c));
-      return num;			/* Return it */
-    } else
-      fprintf(stderr,"Illegal byte in header\n");
-  }
-}
-
-/* Read a PGM file, skipping the CRW -> PGM conversion */
-read_pgm(int in)
-{
-  int width, height, maxval;
-  FILE *fp;
-
-#if O_BINARY != 0
-  fp=fdopen(in,"rb");
-#else
-  fp=fdopen(in,"r");
+#ifdef DEBUG
+  close(gfd);
+  close(mfd);
 #endif
-  width=get_num(fp);
-  height=get_num(fp);
-  maxval=get_num(fp);
 
-  if (width != 854 || height != 613 || maxval != 255)
-  { fprintf(stderr, "PGM file must be 854x613, 256 colors!\n");
-    fclose(fp);
-    return 1;
-  }
-  fread(pgm,854,613,fp);
-  fclose(fp);
-  return 0;
+  return 1;                     /* Success */
 }
 
 /*
@@ -125,12 +202,12 @@ get_rgb(float rgb[3], uchar gmcy[4])
   int r, g;
   static const float coeff[3][4] =
   {
-    { -2.400719,  3.539540, -2.515721,  3.421035 }, /* red */
-#if 0
-    {  4.013642, -1.710916,  0.690795,  0.417247 }, /* green */
-#endif
-    {  2.006821, -0.855458,  0.345397,  0.208623 }, /* green/2 (why??) */
-    { -2.345669,  3.385090,  3.521597, -2.249256 }  /* blue */
+    { -2.400719 * RED_MUL,  3.539540 * RED_MUL,	 /* red from GMCY */
+      -2.515721 * RED_MUL,  3.421035 * RED_MUL },
+    {  4.013642 * GRN_MUL, -1.710916 * GRN_MUL,  /* green from GMCY */
+       0.690795 * GRN_MUL,  0.417247 * GRN_MUL },
+    { -2.345669 * BLU_MUL,  3.385090 * BLU_MUL,  /* blue from GMCY */
+       3.521597 * BLU_MUL, -2.249256 * BLU_MUL }
   };
 
   memset(rgb,0,12);
@@ -139,23 +216,8 @@ get_rgb(float rgb[3], uchar gmcy[4])
       rgb[r] += coeff[r][g] * gmcy[g];
 }
 
-/*
-   Returns the color of a given pixel.  The pattern goes:
 
-(0,0)->	G M G M G M
-	C Y C Y C Y
-	M G M G M G
-	C Y C Y C Y
-*/
-color (unsigned x, unsigned y)
-{
-  int color;
-
-  color = ((y & 1) << 1) + (x & 1);
-  if ((y & 3) == 2)		/* 0=green, 1=magenta, 2=cyan, 3=yellow */
-    color ^= 1;			/* Swap green and magenta */
-  return color;
-}
+#if 0
 
 /*
      For all values of (x,y), the pixels (x,y), (x+1,y), (x,y+1),
@@ -191,37 +253,11 @@ write_ppm(char *fname)
 	ppm[x][c] = val;
       }
     }
-
-/* This moire smoothing doesn't work very well. */
-
-#ifdef MOIRE_SMOOTH
-    for (c=0; c < 3; c++)
-    {
-      prev = ppm[0][c];
-      for (x=1; x < 852; x++)
-      {
-	val = prev + ppm[x][c]*2 + ppm[x+1][c] + 1 >> 2;
-	prev = ppm[x][c];
-	ppm[x][c] = val;
-      }
-    }
-#endif
-
     write (out, ppm, sizeof ppm);
   }
   close(out);
 }
-
-/* Creates a new filename with a different extension */
-exten(char *new, char *old, char *ext)
-{
-  char *cp;
-
-  strcpy(new,old);
-  cp=strrchr(new,'.');
-  if (!cp) cp=new+strlen(new);
-  strcpy(cp,ext);
-}
+#endif
 
 main(int argc, char **argv)
 {
@@ -232,42 +268,19 @@ main(int argc, char **argv)
   if (argc < 2)
   {
     puts("\nCRW to PPM converter by Dave Coffin (dcoffin@shore.net)");
-    puts("Version 0.3, last modified 3/24/97\n");
+    puts("Version 0.4, last modified 8/24/97\n");
     printf("Usage:  %s file1.crw file2.crw ...\n\n",argv[0]);
     exit(1);
   }
 
   for (arg=1; arg < argc; arg++)
   {
-    fd = open(argv[arg],O_RDONLY | O_BINARY);
-    if (fd < 0)
-    { perror(argv[arg]);
-      continue; }
-    read(fd,&magic,2);
-    if (magic == 0x4d4d)		/* "MM" */
+    if (read_crw(argv[arg]))
     {
-      if (read_crw(fd))
-      { fprintf(stderr,"%s is not a CRW file.\n",argv[arg]);
-	continue;
-      }
-
-#ifdef WANT_PGM_FILE
-      exten(fname, argv[arg],".pgm");	/* Write the pixel data to */
-      fd = open(fname,WFLAGS,0644);	/* a PGM file */
-      if (fd < 0)
-      { perror(fname);
-	continue; }
-      write(fd,"P5\n854 613\n255\n",15);
-      write(fd, pgm, 613*854);
-      close(fd);
+      exten(fname, argv[arg],".ppm");	/* Open a PPM file for RGB output */
+#if 0
+      write_ppm(fname);
 #endif
     }
-    else if (ntohs(magic)==0x5035)	/* "P5" */
-    {
-      if (read_pgm(fd)) continue;
-    }
-
-    exten(fname, argv[arg],".ppm");	/* Open a PPM file for RGB output */
-    write_ppm(fname);
   }
 }
