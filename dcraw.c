f9c559c3d5455f09c51aead17e069e59678e8ff4
dcoffin <dcoffin>
1997-08-24 04:06:03 +0000

    Version 0.8 Devised a totally new interpolation algorithm, using a 3x3 block. Run time is much longer, but image quality has vastly improved.

--- dcraw.c
+++ dcraw.c
@@ -1,7 +1,7 @@
 /*
-   CRW to PPM converter Version 0.4
+   CRW to PPM converter Version 0.8
 
-   by Dave Coffin (dcoffin at shore dot net)  8/24/97
+   by Dave Coffin (dcoffin at shore dot net)  8/23/97
 
    No rights reserved.  Do what you want with this code,
    but I accept no responsibility for any consequences
@@ -14,15 +14,15 @@
 #include <stdio.h>
 #include <string.h>
 
-/*  Create extra files with debugging info */
-
 #define DEBUG
 
 /* Use these to adjust the final color balance */
 
 #define RED_MUL 1.0
-#define GRN_MUL 0.5
-#define BLU_MUL 1.0
+#define GRN_MUL 0.535
+#define BLU_MUL 0.709
+
+#define GAMMA 0.75
 
 /* DOS likes to trash binary files!! */
 
@@ -49,141 +49,164 @@ exten(char *new, char *old, char *ext)
   strcpy(cp,ext);
 }
 
-#ifdef DEBUG
-debug (char *arg, char *ext)
-{
-  char fname[64];
-  int fd;
+/*
+   Returns the filter color of a given pixel.
+   The pattern is:
 
-  exten(fname,arg,ext);
-  fd = open(fname,WFLAGS,0644);
-  if (fd < 0)
-  { perror(fname);
-    fd = open("/dev/null",O_WRONLY);
-  }
-  return fd;
-}
-#endif
+	  0 1 2 3 4 5
+	0 G M G M G M		Return values
+	1 C Y C Y C Y		 0  1  2  3
+	2 M G M G M G		 G  M  C  Y
+	3 C Y C Y C Y
+*/
+
+#define filter(row,col) \
+	(0xe1e4 >> ((((row) << 1 & 6) + ((col) & 1)) << 1) & 3)
 
 /*
-   Returns the color of a given pixel.  The pattern goes:
-
-(0,0)->	G M G M G M	row 0
-	C Y C Y C Y	row 1
-	M G M G M G	row 2
-	C Y C Y C Y	row 3
+   Load CCD pixel values into the gmcy[] array.  Unknown colors
+(such as cyan under a magenta filter) must be set to zero.
 */
-color (unsigned x, unsigned y)
+
+read_crw(char *fname)
 {
-  int color;
+  uchar  data[1120], *dp;
+  ushort pixel[896], *pix;
+  int fd, irow, orow, col;
 
-  color = ((y & 1) << 1) + (x & 1);
-  if ((y & 3) == 2)		/* 0=green, 1=magenta, 2=cyan, 3=yellow */
-    color ^= 1;			/* Swap green and magenta on row 2 */
-  return color;
-}
-
-/* Read a CRW file into the pgm[] array */
-read_crw(char *arg)
-{
-  uchar data[1120], *dp, *gp;
-  int fd, irow, icol, orow, ocol;
-
-#ifdef DEBUG
-  int gfd, gcol, mfd, mcol;
-  uchar grid[896], myst[224];
-
-  gfd = debug(arg,".grid");
-  mfd = debug(arg,".myst");
-
-  write(gfd,"P5\n896 613\n255\n",15);
-  write(mfd,"P4\n1792 613\n",12);
-#endif
-
-  fd = open(arg,O_RDONLY | O_BINARY);
+  fd = open(fname,O_RDONLY | O_BINARY);
   if (fd < 0)
-  { perror(arg);
+  { perror(fname);
     return 0; }
 
-  read (fd, data, 26);				/* Check the header */
+/* Check the header to confirm this is a CRW file */
+
+  read (fd, data, 26);
   if (memcmp(data,"MM",2) || memcmp(data+6,"HEAPCCDR",8))
   {
-    fprintf(stderr,"%s is not a CRW file.\n",arg);
+    fprintf(stderr,"%s is not a CRW file.\n",fname);
     return 0;
   }
 
+#ifdef DEBUG
+  fprintf(stderr,"Unpacking %s...\n",fname);
+#endif
+
+/*
+    Immediately after the 26-byte header come the data rows.  First
+the even rows 0..612, then the odd rows 1..611.  Each row is 896
+pixels, ten bits per pixel, packed into 1120 bytes (8960 bits).
+*/
+
   for (irow=orow=0; irow < 613; irow++)
   {
-    read (fd, data, 1120);                      /* Read one row */
+/* Read one packed row */
+    read (fd, data, 1120);
 
-#ifdef DEBUG
-    for (dp=data, gp=grid; dp < data+1120; dp+=10, gp+=8)
+/* Unpack the pixels */
+    for (dp=data, pix=pixel; dp < data+1120; dp+=10, pix+=8)
     {
-/* wrong */
-      gp[0]=(dp[0] << 2) + (dp[1] >> 4 & 3);
-      gp[1]=(dp[2] << 2) + (dp[1] >> 2 & 3);
-      gp[2]=(dp[3] << 2) + (dp[1] >> 0 & 3);
-      gp[3]=(dp[4] << 2) + (dp[1] >> 6 & 3);
-      gp[4]=(dp[5] << 2) + (dp[9] >> 0 & 3);
-      gp[5]=(dp[6] << 2) + (dp[9] >> 2 & 3);
-      gp[6]=(dp[7] << 2) + (dp[9] >> 4 & 3);
-      gp[7]=(dp[8] << 2) + (dp[9] >> 6 & 3);
-
-#if 0
-/* right */
-      gp[0]=(dp[0] << 2) + (dp[1] >> 6 & 3);
-      gp[1]=(dp[2] << 2) + (dp[1] >> 4 & 3);
-      gp[2]=(dp[3] << 2) + (dp[1] >> 2 & 3);
-      gp[3]=(dp[4] << 2) + (dp[1] >> 0 & 3);
-      gp[4]=(dp[5] << 2) + (dp[9] >> 0 & 3);
-      gp[5]=(dp[6] << 2) + (dp[9] >> 2 & 3);
-      gp[6]=(dp[7] << 2) + (dp[9] >> 4 & 3);
-      gp[7]=(dp[8] << 2) + (dp[9] >> 6 & 3);
-#endif
+      pix[0] = (dp[0] << 2) + (dp[1] >> 6    );
+      pix[1] = (dp[2] << 2) + (dp[1] >> 4 & 3);
+      pix[2] = (dp[3] << 2) + (dp[1] >> 2 & 3);
+      pix[3] = (dp[4] << 2) + (dp[1]      & 3);
+      pix[4] = (dp[5] << 2) + (dp[9]      & 3);
+      pix[5] = (dp[6] << 2) + (dp[9] >> 2 & 3);
+      pix[6] = (dp[7] << 2) + (dp[9] >> 4 & 3);
+      pix[7] = (dp[8] << 2) + (dp[9] >> 6    );
     }
-    lseek (gfd, orow*896+15, 0);
-    write (gfd, grid, 896);
-    lseek (mfd, orow*224+12, 0);
-    write (mfd, myst, 224);
-#endif
 
-#if 0
-    for (icol=gcol=mcol=0; icol < 1120; icol++)
-    {
-      if ( icol % 10 != 1 && icol % 10 != 9 )
-	grid[gcol++] = data[icol];		/* Data pixels go here */
-      else
-	myst[mcol++] = data[icol];		/* Mystery pixels go here */
-    }
-    lseek (gfd, orow*896+15, 0);
-    write (gfd, grid, 896);
-    lseek (mfd, orow*224+12, 0);
-    write (mfd, myst, 224);
-#endif
+/*
+   Copy 854 pixels into the gmcy[] array.  The other 42 pixels
+are blank.  Left-shift by 4 for extra precision in upcoming
+calculations.
+*/
+    memset(gmcy[orow], 0, 854 * 8);	/* Set row to zero */
+    for (col=0; col < 854; col++)
+      gmcy[orow][col][filter(orow,col)] = pixel[col] << 4;
 
-    for (icol=ocol=0; icol < 1067; icol++)
-    {
-      if ( icol % 10 != 1 && icol % 10 != 9 )
-      {
-	gmcy[orow][ocol][color(orow,ocol)] = (ushort) data[icol] << 6;
-	ocol++;
-      }
-    }
     if ((orow+=2) > 612)        /* Once we've read all the even rows, */
       orow = 1;                 /* read the odd rows. */
   }
   close(fd);
-
-#ifdef DEBUG
-  close(gfd);
-  close(mfd);
-#endif
-
   return 1;                     /* Success */
 }
 
 /*
-   Converts a GMCY quadruplet into an RGB triplet.
+   When this function is called, we only have one GMCY
+value for each pixel.  Do linear interpolation to get
+the other three.
+*/
+
+first_interpolate()
+{
+  int y, x, sy, sx, c;
+  uchar shift[]={ 2,1,2, 1,16,1, 2,1,2 }, *sp;
+
+#ifdef DEBUG
+  fprintf(stderr,"First interpolation...\n");
+#endif
+
+  for (y=1; y < 612; y++)
+  {
+    for (x=1; x < 853; x++)
+    {
+      sp=shift;
+      for (sy=y-1; sy < y+2; sy++)
+	for (sx=x-1; sx < x+2; sx++)
+	{
+	  c=filter(sy,sx);
+	  gmcy[y][x][c] += gmcy[sy][sx][c] >> *sp++;
+	}
+    }
+  }
+}
+
+/*
+   We now have all four GMCY values for each pixel.  Smooth
+the color balance to avoid artifacts.  This function may be
+called more than once.
+*/
+
+second_interpolate()
+{
+  ushort data[2][854][4];
+  ushort (*last_row)[4]=data[0];
+  ushort (*this_row)[4]=data[1];
+  void *tmp;
+
+  int y, x, c, sy, sx, sc;
+  uchar shift[]={ 2,1,2, 1,0,1, 2,1,2 }, *sp;
+
+#ifdef DEBUG
+  fprintf(stderr,"Second interpolation...\n");
+#endif
+
+  for (y=2; y < 611; y++)
+  {
+    memset(this_row, 0, 854*8);
+    for (x=2; x < 852; x++)
+    {
+      sp=shift;
+      c=filter(y,x);
+      for (sy=y-1; sy < y+2; sy++)
+	for (sx=x-1; sx < x+2; sx++)
+	{
+	  sc=filter(sy,sx);
+	  this_row[x][sc] +=
+	   ( (unsigned long) gmcy[sy][sx][sc] << 16)
+	    / gmcy[sy][sx][c] * gmcy[y][x][c] >> (16 + *sp++);
+	}
+    }
+    if (y > 2) memcpy(gmcy[y-1]+2,last_row+2,850*8);
+    tmp = last_row;
+    last_row = this_row;
+    this_row = tmp;
+  }
+}
+
+/*
+   Convert a GMCY quadruplet to an RGB triplet.
 
    The following table shows how the four CCD pixel types respond
 to the three primary colors, on a scale of 0-100.
@@ -197,7 +220,8 @@ to the three primary colors, on a scale of 0-100.
 
    get_rgb() is based on this table.
 */
-get_rgb(float rgb[3], uchar gmcy[4])
+
+get_rgb(float rgb[3], ushort gmcy[4])		/* 3.70 seconds */
 {
   int r, g;
   static const float coeff[3][4] =
@@ -216,59 +240,98 @@ get_rgb(float rgb[3], uchar gmcy[4])
       rgb[r] += coeff[r][g] * gmcy[g];
 }
 
-
-#if 0
-
 /*
-     For all values of (x,y), the pixels (x,y), (x+1,y), (x,y+1),
-and (x+1,y+1) form a GMCY vector (i.e. no two have the same color).
-Convert these vectors to RGB, then try to remove the artifacts that
-appear when there's an edge between x and x+1.
+   Convert the GMCY grid to RGB and write it to a PPM file.
 */
+
 write_ppm(char *fname)
 {
-  int out, y, x, sy, sx, c, val, prev;
-  uchar gmcy[4], ppm[853][3];
-  float rgb[3];
+  int fd, y, x, i;
+  register c, val;
+  uchar ppm[854][3];
+  float rgb[3], max, gamma, gamtab[257];
+  int histo[256], total, bot, mid, top;
 
-  out = open(fname,WFLAGS,0644);
-  if (out < 0)
-  { perror(fname);
-    return; }
-  write(out,"P6\n853 612\n255\n",15);
+#ifdef DEBUG
+  fprintf(stderr,"First pass RGB...\n");
+#endif
 
-  for (y=0; y < 612; y++)
+/*
+   First pass:  Needed to find the maximum color value.
+   Construct a histogram with binning of 1024.
+*/
+  memset(histo,0,sizeof histo);
+  for (y=2; y < 611; y++)
   {
-    for (x=0; x < 853; x++)
+    for (x=2; x < 852; x++)
     {
-      for (sy=y; sy < y+2; sy++)
-	for (sx=x; sx < x+2; sx++)
-	  gmcy[color(sx,sy)] = pgm[sy][sx];
-      get_rgb(rgb,gmcy);
+      get_rgb(rgb,gmcy[y][x]);
       for (c=0; c < 3; c++)
       {
-	val = floor(rgb[c]);
-	if (val < 0) val=0;
-	if (val > 255) val=255;
-	ppm[x][c] = val;
+	histo [(int)rgb[c] >> 10] ++;
       }
     }
-    write (out, ppm, sizeof ppm);
   }
-  close(out);
-}
+/*
+   Set the maximum to the 95th percentile
+*/
+  for (val=256, total=0; --val; )
+    if ((total+=histo[val]) > 80000) break;
+  max = val << 10;
+
+  gamma = GAMMA;
+  for (i=0; i < 257; i++)
+    gamtab[i] = pow(i/256.0,1/gamma) * max;
+
+#ifdef DEBUG
+  fprintf(stderr,"Second pass RGB...\n");
 #endif
+
+  fd = open(fname,WFLAGS,0644);
+  if (fd < 0)
+  { perror(fname);
+    return; }
+  write(fd,"P6\n852 611\n255\n",15);
+
+/*
+   Second pass:  Scale RGB and write to PPM file
+*/
+  for (y=1; y < 612; y++)		/* 4.93 seconds */
+  {
+    for (x=1; x < 853; x++)
+    {
+      get_rgb(rgb,gmcy[y][x]);
+      for (c=0; c < 3; c++)
+      {
+	for (bot=0,top=256; top-bot > 1; )	/* 3.00 seconds */
+	{
+	  mid = bot+top >> 1;		/* Do a binary search */
+	  if (rgb[c] > gamtab[mid])
+	    bot=mid;
+	  else
+	    top=mid;
+	}
+	ppm[x][c]=bot;
+      }
+    }
+    write (fd, ppm+1, 852 * 3);
+  }
+  close(fd);
+
+#ifdef DEBUG
+  fprintf(stderr,"Done!\n");
+#endif
+}
 
 main(int argc, char **argv)
 {
   char fname[256];
-  int arg, fd, x, y;
-  short magic;
+  int arg;
 
   if (argc < 2)
   {
     puts("\nCRW to PPM converter by Dave Coffin (dcoffin@shore.net)");
-    puts("Version 0.4, last modified 8/24/97\n");
+    puts("Version 0.8, last modified 8/23/97\n");
     printf("Usage:  %s file1.crw file2.crw ...\n\n",argv[0]);
     exit(1);
   }
@@ -277,10 +340,10 @@ main(int argc, char **argv)
   {
     if (read_crw(argv[arg]))
     {
-      exten(fname, argv[arg],".ppm");	/* Open a PPM file for RGB output */
-#if 0
+      first_interpolate();
+      second_interpolate();
+      exten(fname, argv[arg],".ppm");
       write_ppm(fname);
-#endif
     }
   }
 }
