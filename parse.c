7fc8487089e0337c9bba6a85f2e9ffef1e5235d5
dcoffin <dcoffin>
2005-04-27 19:51:22 +0000

    Don't generate gigantic thumbnails for Leaf images.

--- parse.c
+++ parse.c
@@ -6,8 +6,8 @@
    from any raw digital camera formats that have them, and
    shows table contents.
 
-   $Revision: 1.32 $
-   $Date: 2005/04/22 16:03:53 $
+   $Revision: 1.33 $
+   $Date: 2005/04/27 19:51:22 $
  */
 
 #include <stdio.h>
@@ -401,7 +401,7 @@ void parse_tiff (int base)
     spp = 1;
     thumb_layers = 0;
   }
-  if (!thumb_length) {
+  if (!thumb_length && offset) {
     thumb_offset = offset;
     sprintf (thumb_head, "P%d\n%d %d\n%d\n",
 	spp > 1 ? 6:5, width, height, (1 << bps) - 1);
