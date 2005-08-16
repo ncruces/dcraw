72e43228037ff762dc753e3f3f77f31c9713008b
dcoffin <dcoffin>
2005-08-16 08:53:01 +0000

    Added "mdc" extension.

--- rawphoto.c
+++ rawphoto.c
@@ -3,8 +3,8 @@
    by Dave Coffin at cybercom dot net, user dcoffin
    http://www.cybercom.net/~dcoffin/
 
-   $Revision: 1.23 $
-   $Date: 2005/07/22 22:37:39 $
+   $Revision: 1.24 $
+   $Date: 2005/08/16 08:53:01 $
 
    This code is licensed under the same terms as The GIMP.
    To simplify maintenance, it calls my command-line "dcraw"
@@ -43,7 +43,7 @@
 #include <libintl.h>
 #define _(String) gettext(String)
 
-#define PLUG_IN_VERSION  "1.1.11 - 22 July 2005"
+#define PLUG_IN_VERSION  "1.1.12 - 16 August 2005"
 
 static void query(void);
 static void run(RAWPHOTO_CONST gchar *name,
@@ -106,7 +106,7 @@ static void query (void)
 			  load_return_vals);
 
   gimp_register_load_handler ("file_rawphoto_load",
-    "bay,bmq,cr2,crw,cs1,dc2,dcr,dng,erf,fff,hdr,jpg,k25,kdc,mos,mrw,nef,orf,pef,pxn,raf,raw,rdc,srf,tif,x3f", "");
+    "bay,bmq,cr2,crw,cs1,dc2,dcr,dng,erf,fff,hdr,jpg,k25,kdc,mdc,mos,mrw,nef,orf,pef,pxn,raf,raw,rdc,srf,tif,x3f", "");
 }
 
 static void run (RAWPHOTO_CONST gchar *name,
