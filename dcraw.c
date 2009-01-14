4a7980725d075d20ef4fdc357618e3789eb17d79
dcoffin <dcoffin>
2009-01-14 17:46:50 +0000

    Support the Samsung S85 at ISO 800 and higher.

--- dcraw.c
+++ dcraw.c
@@ -19,8 +19,8 @@
    *If you have not modified dcraw.c in any way, a link to my
    homepage qualifies as "full source code".
 
-   $Revision: 1.414 $
-   $Date: 2009/01/14 03:23:23 $
+   $Revision: 1.415 $
+   $Date: 2009/01/14 17:46:50 $
  */
 
 #define VERSION "8.90"
@@ -6575,6 +6575,7 @@ void CLASS identify()
     {  6114240, "PENTAX",   "Optio S4"        ,1 },  /* or S4i, CASIO EX-Z4 */
     { 10702848, "PENTAX",   "Optio 750Z"      ,1 },
     { 16098048, "SAMSUNG",  "S85"             ,1 },
+    { 16215552, "SAMSUNG",  "S85"             ,1 },
     { 12582980, "Sinar",    ""                ,0 },
     { 33292868, "Sinar",    ""                ,0 },
     { 44390468, "Sinar",    ""                ,0 } };
@@ -7242,10 +7243,10 @@ konica_400z:
   } else if (!strcmp(model,"S85")) {
     height = 2448;
     width  = 3264;
-    raw_width = 3288;
+    raw_width = fsize/height/2;
     order = 0x4d4d;
     load_raw = &CLASS unpacked_load_raw;
-    maximum = 0xfef8;
+    maximum = 0xffff;
   } else if (!strcmp(model,"STV680 VGA")) {
     height = 484;
     width  = 644;
