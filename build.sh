c6d15fe510c0b48ae7a9b3f865470bfd51ad8310
Nuno Cruces <ncruces@users.noreply.github.com>
2022-12-22 14:12:47 +0000

    Build action.

--- build.sh
+++ build.sh
@@ -2,49 +2,40 @@
 
 set -Eeuo pipefail
 
-JVER=2.1.4
+JVER=2.1.5.1
 if [ ! -d "libjpeg-turbo-$JVER" ]; then
   url="https://downloads.sourceforge.net/project/libjpeg-turbo/$JVER/libjpeg-turbo-$JVER.tar.gz"
-  curl -sL "$url" | tar xz
+  curl -sL "$url" | tar vxz
 fi
 
-if [[ "$OSTYPE" == "darwin"* ]]; then
-
-mkdir -p build_arm
-pushd build_arm
-cmake -G"Unix Makefiles" "../libjpeg-turbo-$JVER" \
-  -D"CMAKE_OSX_DEPLOYMENT_TARGET=11" \
-  -D"CMAKE_OSX_ARCHITECTURES=arm64"
-make -j jpeg-static
-cc -O3 -o dcraw ../dcraw.c -DNO_JASPER -DNO_LCMS \
-   -I. -I"../libjpeg-turbo-$JVER" libjpeg.a \
-   --target=arm64-apple-macos11
-popd
-
-mkdir -p build_x86
-pushd build_x86
-cmake -G"Unix Makefiles" "../libjpeg-turbo-$JVER" \
-  -D"CMAKE_OSX_DEPLOYMENT_TARGET=10.13" \
-  -D"CMAKE_OSX_ARCHITECTURES=x86_64"
-make -j jpeg-static
-cc -O3 -o dcraw ../dcraw.c -DNO_JASPER -DNO_LCMS \
-   -I. -I"../libjpeg-turbo-$JVER" libjpeg.a \
-   --target=x86_64-apple-macos10.13
-popd
-
-lipo -create -output dcraw build_arm/dcraw build_x86/dcraw
-
-else
-
-mkdir -p build
-
-pushd build
-cmake -G"Unix Makefiles" "../libjpeg-turbo-$JVER"
-make -j jpeg-static
-cc -O3 -o ../dcraw ../dcraw.c -lm -DNO_JASPER -DNO_LCMS \
-   -I. -I"../libjpeg-turbo-$JVER" libjpeg.a
-popd
-
+ZVER=0.10.0
+if [ ! -d "zig-linux-x86_64-$ZVER" ]; then
+  url="https://ziglang.org/download/$ZVER/zig-linux-x86_64-$ZVER.tar.xz"
+  curl -sL "$url" | tar vxJ
 fi
+PATH="$PWD/zig-linux-x86_64-$ZVER:$PATH"
 
-tar c dcraw | gzip -9 > dcraw.tgz
+BVER=version_112
+if [ ! -d "binaryen-$BVER" ]; then
+  url="https://github.com/WebAssembly/binaryen/releases/download/$BVER/binaryen-$BVER-x86_64-linux.tar.gz"
+  curl -sL "$url" | tar vxz
+fi
+PATH="$PWD/binaryen-$BVER/bin:$PATH"
+
+mkdir -p build_wasm
+pushd build_wasm
+CC="zig cc --target=wasm32-wasi" cmake -G"Unix Makefiles" "../libjpeg-turbo-$JVER" \
+  -D"CMAKE_C_FLAGS_RELEASE=-Oz -g0 -DNDEBUG" \
+  -D"WITH_SIMD=0" \
+  -D"WITH_TURBOJPEG=0"
+
+make -j jpeg-static
+zig cc --target=wasm32-wasi -g0 -flto \
+       -Oz -o dcraw.wasm ../dcraw.c -DNO_JASPER -DNO_LCMS \
+       -I. -I"../libjpeg-turbo-$JVER" \
+       CMakeFiles/jpeg-static.dir/*.o \
+       CMakeFiles/simd.dir/*.o
+popd
+
+wasm-opt build_wasm/dcraw.wasm -O4 -o dcraw.wasm
+gzip -9f dcraw.wasm
